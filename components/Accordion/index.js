5e51f07f4540b2cb60b34908a497a1abc091e03e
Bagna Farouk Omar <91009147+omarbagna@users.noreply.github.com>
2023-08-11 20:50:22 +0000

    ability to minimize all accordions

--- components/Accordion/index.js
+++ components/Accordion/index.js
@@ -1,33 +1,37 @@
-import React, { useState } from "react";
-import AccordionItem from "./AccordionItem";
+import React, { useState } from 'react';
+import AccordionItem from './AccordionItem';
 
 const Accordion = ({ questionsAnswers }) => {
-  const [activeIndex, setActiveIndex] = useState(0);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-  const renderedQuestionsAnswers = questionsAnswers.map((item, index) => {
-    const showDescription = index === activeIndex ? "show-description" : "";
-    const fontWeightBold = index === activeIndex ? "font-weight-bold" : "";
-    const ariaExpanded = index === activeIndex ? "true" : "false";
-    return (
-      <AccordionItem
-        key={index}
-        showDescription={showDescription}
-        fontWeightBold={fontWeightBold}
-        ariaExpanded={ariaExpanded}
-        item={item}
-        index={index}
-        onClick={() => {
-          setActiveIndex(index);
-        }}
-      />
-    );
-  });
+	const handleExpand = (index) => {
+		setActiveIndex((prev) => (prev === index ? null : index));
+	};
 
-  return (
-    <div className="faq">
-      <dl className="faq__list">{renderedQuestionsAnswers}</dl>
-    </div>
-  );
+	const renderedQuestionsAnswers = questionsAnswers.map((item, index) => {
+		const showDescription = index === activeIndex ? 'show-description' : '';
+		const fontWeightBold = index === activeIndex ? 'font-weight-bold' : '';
+		const ariaExpanded = index === activeIndex ? 'true' : 'false';
+		return (
+			<AccordionItem
+				key={index}
+				showDescription={showDescription}
+				fontWeightBold={fontWeightBold}
+				ariaExpanded={ariaExpanded}
+				item={item}
+				index={index}
+				onClick={() => {
+					handleExpand(index);
+				}}
+			/>
+		);
+	});
+
+	return (
+		<div className="faq">
+			<dl className="faq__list">{renderedQuestionsAnswers}</dl>
+		</div>
+	);
 };
 
 export default Accordion;
