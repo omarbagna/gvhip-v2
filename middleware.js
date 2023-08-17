09ac3a6bf4448792dc84b5e13854bc1c33545acc
Bagna Farouk Omar <91009147+omarbagna@users.noreply.github.com>
2023-08-17 11:58:56 +0000

    fixed middleware authorization

--- middleware.js
+++ middleware.js
@@ -4,26 +4,34 @@ import { NextResponse } from 'next/server';
 export default withAuth(
 	function middleware(req) {
 		if (
-			req.nextUrl.pathname.startsWith('/company') &&
+			(req.nextUrl.pathname.startsWith('/immigration') ||
+				req.nextUrl.pathname.startsWith('/guest') ||
+				req.nextUrl.pathname.startsWith('/policy-holder')) &&
 			req.nextauth.token?.user?.role === 'company'
 		) {
 			return NextResponse.redirect(new URL('/company', req.url));
 		}
 
 		if (
-			req.nextUrl.pathname.startsWith('/immigration') &&
+			(req.nextUrl.pathname.startsWith('/company') ||
+				req.nextUrl.pathname.startsWith('/guest') ||
+				req.nextUrl.pathname.startsWith('/policy-holder')) &&
 			req.nextauth.token?.user?.role === 'immigration'
 		) {
 			return NextResponse.redirect(new URL('/immigration', req.url));
 		}
 		if (
-			req.nextUrl.pathname.startsWith('/guest') &&
+			(req.nextUrl.pathname.startsWith('/company') ||
+				req.nextUrl.pathname.startsWith('/immigration') ||
+				req.nextUrl.pathname.startsWith('/policy-holder')) &&
 			req.nextauth.token?.user?.role === 'guest'
 		) {
 			return NextResponse.redirect(new URL('/guest', req.url));
 		}
 		if (
-			req.nextUrl.pathname.startsWith('/policy-holder') &&
+			(req.nextUrl.pathname.startsWith('/company') ||
+				req.nextUrl.pathname.startsWith('/immigration') ||
+				req.nextUrl.pathname.startsWith('/guest')) &&
 			req.nextauth.token?.user?.role === 'policy_holder'
 		) {
 			return NextResponse.redirect(new URL('/policy-holder', req.url));
